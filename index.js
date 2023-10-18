53a4b9b60ce24fc12f99f043de2c29852a12a024
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-10-18 00:22:21 -0700

    done two methods for getting user record using query and by name

--- index.js
+++ index.js
@@ -16,84 +16,137 @@ let users = [
 ];
 
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+// app.get('/', (req, res) => {
+//     res.send("Hello World");
+// });
+
+// // Endpoint to retrieve all users
+// app.get('/api/users', (req, res) => {
+//     res.json(users);
+// });
+
+// // Endpoint to create a new user
+// app.post('/api/users', (req, res) => {
+//     const newUser = {
+//         id: users.length + 1,
+//         name: req.body.name,
+//     };
+
+//     users.push(newUser);
+//     res.status(201).json(newUser);
+// });
+
+// // Endpoint to retrieve a user by ID
+// app.get('/api/users/:id', (req, res) => {
+//     const userId = parseInt(req.params.id);
+//     const user = users.find((u) => u.id == userId);
+
+//     if (!user) {
+//         res.status(404).json({ error: 'User not found' });
+//     } else {
+//         res.json(user);
+//     }
+
+// });
+
+// Endpoint to retrieve a user by name
+// Option 1: Retrieve a user by name using a path parameter
+app.get('/api/users/:name', (req, res) => {
+    const userName = req.params.name;
+
+    // Find the user with the specified name
+    const user = users.find(user => user.name === userName);
+
+    if (user) {
+        res.status(200).json(user); // Respond with the user's information
+    } else {
+        res.status(404).json({ error: `${userName} not found` });
+    }
 });
 
-// Endpoint to retrieve all users
+// Option 2: Retrieve a user by name using a query parameter
 app.get('/api/users', (req, res) => {
-    res.json(users);
-});
+    const userName = req.query.name;
 
-// Endpoint to create a new user
-app.post('/api/users', (req, res) => {
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-    };
-
-    users.push(newUser);
-    res.status(201).json(newUser);
-});
-
-// Endpoint to retrieve a user by ID
-app.get('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find((u) => u.id == userId);
-
-    if (!user) {
-        res.status(404).json({ error: 'User not found' });
-    } else {
-        res.json(user);
+    if (!userName) {
+        res.status(400).json({ error: 'Name parameter is required' });
+        return;
     }
 
-});
+    const user = users.find(user => user.name === userName);
 
-// Endpoint to delete all users
-app.delete('/api/users', (req, res) => {
-    // clear the array to delete all users
-    users = [];
-
-    res.status(204).send(); // Respond with a "No Content" status   indicating a successful deletion
-
-});
-
-// Endpoint to delete a specific user by ID
-app.delete('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-
-    // Find the index of the user with the specified ID
-    const userIndex = users.findIndex((user) => user.id === userId);
-
-    if (userIndex === -1) {
-        res.status(404).json({error: 'User not found'});
+    // if user found
+    if (user) {
+        res.status(200).json(user);
     } else {
-        // Remove the user form the array
-        users.splice(userIndex, 1);
-        res.send(204).send();   // Respond with a "No Content" status
-    }
-
-});
-
-
-// Endpoint to update a specific resourse by ID
-app.put('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-
-    // check if the resource with specific ID exists
-    const existingResource = users.find((user) => user.id === userId);
-
-    if (!existingResource) {
-        res.status(404).json({error: 'User not found'});
-    } else {
-        // Update the resource with the data from the request body
-        existingResource.id = req.body.id;
-        existingResource.name = req.body.name;
-        // Update other fields...
-
-        res.status(200).json(existingResource); // Respond with the updated resource
+        res.status(404).json({ error: `${userName} not found` });
     }
 });
+
+
+
+// // Endpoint to delete all users
+// app.delete('/api/users', (req, res) => {
+//     // clear the array to delete all users
+//     users = [];
+
+//     res.status(204).send(); // Respond with a "No Content" status   indicating a successful deletion
+
+// });
+
+// // Endpoint to delete a specific user by ID
+// app.delete('/api/users/:id', (req, res) => {
+//     const userId = parseInt(req.params.id);
+
+//     // Find the index of the user with the specified ID
+//     const userIndex = users.findIndex((user) => user.id === userId);
+
+//     if (userIndex === -1) {
+//         res.status(404).json({ error: 'User not found' });
+//     } else {
+//         // Remove the user form the array
+//         users.splice(userIndex, 1);
+//         res.send(204).send();   // Respond with a "No Content" status
+//     }
+
+// });
+
+// // Endpoint to delete a specific user by name
+// app.delete('/api/users/:name', (req, res) => {
+//     const userName = req.params.name;
+
+//     // Filter the users array to find users with the specified name
+//     const userToDelete = users.find((user) => user.name === userName);
+
+//     if (!userToDelete) {
+//         res.status(404).json({ error: 'User with the specific name not found' });
+//     } else {
+//         // Remove the found user from the array
+//         users = users.filter((user) => user.name !== userName);
+//         res.status(204).send();
+//     }
+// });
+
+
+
+// // Endpoint to update a specific resourse by ID
+// app.put('/api/users/:id', (req, res) => {
+//     const userId = parseInt(req.params.id);
+
+//     // check if the resource with specific ID exists
+//     const existingResource = users.find((user) => user.id === userId);
+
+//     if (!existingResource) {
+//         res.status(404).json({ error: 'User not found' });
+//     } else {
+//         // Update the resource with the data from the request body
+//         existingResource.id = req.body.id;
+//         existingResource.name = req.body.name;
+//         // Update other fields...
+
+//         res.status(200).json(existingResource); // Respond with the updated resource
+//     }
+// });
 
 
 app.listen(Port, () => {
